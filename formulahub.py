e5eee73261a68592d92b0d8136903f27d8459251
zzzacL <41882760+zzzacL@users.noreply.github.com>
2018-08-01 15:33:36 -0400

    cleaned up the command line
    added clear() into the program to allow the program to appear a lot more clean and "professional"
    also added the logo to every screen lol

--- formulahub.py
+++ formulahub.py
@@ -1,16 +1,20 @@
 import math
+import os
+from time import sleep
+
+def clear():
+    if opsys == "win":
+        _ = os.system('cls')
+    else:
+        _ = os.system('clear')
 
 def logo():
     print ("████  ████  ███   ██ ██  █  █  █      ██   █  █  █  █  ███ ")
     print ("█     █  █  █  █  █ █ █  █  █  █     █  █  █  █  █  █  █ █ ")
     print ("███   █  █  ███   █   █  █  █  █     ████  ████  █  █  █  █")
-    print ("█     ████  █  █  █   █   ███  ████  █  █  █  █   ███  ███  v0.05")
+    print ("█     ████  █  █  █   █   ███  ████  █  █  █  █   ███  ███  v0.06")
 
 logo()
-
-print ("This program will display a list of formulas to complete mathematical operations.")
-print ("Select the name of the formula you require.")
-print ("")
 
 def salist():
     print ("Surface Area:")
@@ -18,7 +22,7 @@ def salist():
     print ("circle · · · · · · : A = πr^2")
     print ("trapezoid  · · · · : A = 1/2(b1 + b2)h")
     print ("sphere · · · · · · : S = 4πr^2")
-
+    
 def vollist():
     print ("Volume Formulas:")
     print ("cube · · · · · · · : V = S^3")
@@ -27,16 +31,16 @@ def vollist():
     print ("cylinder · · · · · : V = πr^3 * h")
     print ("cone · · · · · · · : V = 1/3πr^2 * h")
     print ("sphere · · · · · · : V = 4/3πr^3")
-
+    
 def trilist():
     print ("Trig Formulas:")
     print ("pythagorean theorem: a^2 + b^2 = c^2")
-
+    
 def vislist():
     print ("Visualizers:")
     print ("30-60-90 Visualizer: '369'")
     print ("45-45-90 Visualizer: '449'")
-
+    
 def complist():
     salist()
     print ("")
@@ -62,24 +66,17 @@ def rtri369():
     print ("")
 
 def prove369():
-        if a == o*2:
-            rtri369()
-        else:
-            print ("That is not a 30° 60° 90° triangle.")
-
-def prove449():
-        if a == o:
-            rtri449()
-        else:
-            print ("That is not a 45° 45° 90° triangle.")
-
+    if a == o*2:
+        rtri369()
+    else:
+        print ("That is not a 30° 60° 90° triangle.")
 
 def rtri449():
     print ("")
     print (str(o) + " u")
     print ("█▀▄")
-    print ("█  ▀▄")
-    print ("█60° ▀▄ " + str(h) + " u")
+    print ("█60▀▄")
+    print ("█    ▀▄ " + str(h) + " u")
     print ("█      ▀▄")
     print ("█        ▀▄")
     print ("█▀▀▀▀█     ▀▄")
@@ -88,11 +85,27 @@ def rtri449():
     print ("        " + str(a) + " u")
     print ("")
 
+def prove449():
+    if a == o:
+        rtri449()
+    else:
+        print ("That is not a 45° 45° 90° triangle.")
+
+def resetprog():
+    input("Press enter to continue")
+    clear()
+    logo()
+    complist()
+
+
 
 complist()
 
 print ("Type '#help' for commands.")
 print ("")
+
+opsys = input("Win/Lin/Mac: ")
+opsys = opsys.lower()
 
 while True:
     form = input("Input: ")
@@ -100,12 +113,14 @@ while True:
 
 #trig list
     if form == "pythagorean theorem":
+            clear()
             u = input("Unit type: ")
             a = float(input("Side A: "))
             b = float(input("Side B: "))
-            h = math.sqrt(a**2 + b**2)
+            h = (a**2 + b**2)**.5
             h = round(h,4)
             print ("Hypotenuse: " + str(h) + " " + u)
+            resetprog()
 
 #surface area list
     elif form == "triangle":
@@ -115,6 +130,7 @@ while True:
             a = (b*h)/2
             a = round(a,4)
             print ("Area: " + str(a) + " " + u + " squared")
+            resetprog()
 
     elif form == "circle":
             u = input("Unit type: ")
@@ -122,6 +138,7 @@ while True:
             a = math.pi*r*r
             a = round(a,4)
             print ("Area: " + str(a) + " " + u + " squared")
+            resetprog()
 
     elif form == "trapezoid":
             u = input("Unit type: ")
@@ -131,6 +148,7 @@ while True:
             a = (h*b1*b2)/2
             a = round(a,4)
             print ("Area: " + str(a) + " " + u + " squared")
+            resetprog()
 
     elif form == "sphere":
             u = input("Unit type: ")
@@ -138,7 +156,8 @@ while True:
             s = (4/3)*math.pi*r**3
             s = round(s,4)
             print ("Surface Area: " + str(s) + " " + u + " squared")
-
+            resetprog()
+            
 #volume list
     elif form == "cube":
             u = input("Unit type: ")
@@ -146,6 +165,7 @@ while True:
             s = s**3
             s = round(s,4)
             print ("Volume: " + str(s) + " " + u + " cubed")
+            resetprog()
 
     elif form == "rectangular prism":
             u = input("Unit type: ")
@@ -155,6 +175,7 @@ while True:
             v = l*w*h
             v = round(v,4)
             print ("Volume: " + str(v) + " " + u + " cubed")
+            resetprog()
 
     elif form == "sqare pyramid":
             u = input("Unit type: ")
@@ -163,7 +184,8 @@ while True:
             v = h*((1/3)*b**2)
             v = round(v,4)
             print ("Volume: " + str(v) + " " + u + " cubed")
-
+            resetprog()
+            
     elif form == "cylinder":
             u = input("Unit type: ")
             r = int(input("Radius: "))
@@ -171,7 +193,8 @@ while True:
             v = h*(math.pi*(r**2))
             v = round(v,4)
             print ("Volume: " + str(v) + " " + u + " cubed")
-
+            resetprog()
+            
     elif form == "cone":
             u = input("Unit type: ")
             h = int(input("Height: "))
@@ -179,29 +202,31 @@ while True:
             v = h*(math.pi*(r**3))/3
             v = round(v,4)
             print ("Volume: " + str(v) + " " + u + " cubed")
-
+            resetprog()
+            
     elif form == "sphere":
             u = input("Unit type: ")
             v = round(v,4)
             print ("Volume: " + str(v) + " " + u + " cubed")
+            resetprog()
 
 #visuzalizer list
     elif form == "369":
             o = int(input("Opposite: "))
             a = int(input("Adjacent: "))
-            h = math.sqrt(o**2 + a**2)
-            h = round(h,4)
+            h = int(input("Hypotenuse: "))
             prove369()
 
     elif form == "449":
             o = int(input("Opposite: "))
             a = int(input("Adjacent: "))
-            h = math.sqrt(o**2 + a**2)
-            h = round(h,4)
+            h = int(input("Hypotenuse: "))
             prove449()
 
 #help list
     elif form == "#help":
+            clear()
+            logo()
             print ("")
             print ("#help: Brings up this list.")
             print ("#logo: brings up the sick Unicode block logo")
@@ -210,28 +235,43 @@ while True:
             print ("#vollist: Brings up Volume Formula list")
             print ("#trilist: Brings up Trig Formula list")
             print ("#vislist: Brings up Visualizer list")
-            print ("Press Enter to exit.")
             print ("")
 
     elif form == "#list":
-            complist()
+        clear()
+        logo()
+        complist()
 
     elif form == "#logo":
-            logo()
+        clear()
+        logo()
+
+    elif form == "#info":
+        clear()
+        print ("This program will display a list of formulas to complete mathematical operations.")
+        print ("Select the name of the formula you require.")
+        print ("")
+        
 
     elif form == "#salist":
-            salist()
+        clear()
+        logo()
+        salist()
 
     elif form == "#vollist":
-            vollist()
+        clear()
+        logo()
+        vollist()
 
     elif form == "#trilist":
-            trilist()
+        clear()
+        logo()
+        trilist()
 
     elif form == "#vislist":
-            vislist()
+        clear()
+        logo()
+        vislist()
 
     else:
-        print ("Please select from the #list.")
-
-input("Press Enter to exit.")
+        print ("Please select from #help or #list.")
