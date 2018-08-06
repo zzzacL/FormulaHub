c3155dfd55551e033c0d634c1819f4ce5c7ed04f
zzzacL <41882760+zzzacL@users.noreply.github.com>
2018-08-06 16:03:08 -0400

    v0.09
    Check the readme :)

--- formulahub.py
+++ formulahub.py
@@ -1,37 +1,47 @@
+#!/usr/bin/env python3
+
 import math
 import os
+from sys import platform
+
 
 def logo():
     print ("""
 ████  ████  ███   ██ ██  █  █  █      ██   █  █  █  █  ███
 █     █  █  █  █  █ █ █  █  █  █     █  █  █  █  █  █  █ █
 ███   █  █  ███   █   █  █  █  █     ████  ████  █  █  █  █
-█     ████  █  █  █   █   ███  ████  █  █  █  █   ███  ███  v0.08.5
+█     ████  █  █  █   █   ███  ████  █  █  █  █   ███  ███  v0.09
 """)
 
 logo()
 
 def salist():
-    print ("""Surface Area:
-traingle · · · · · : A = 1/2bh
-circle · · · · · · : A = πr^2
-trapezoid  · · · · : A = 1/2(b1 + b2)h
-sphere · · · · · · : S = 4πr^2
+    print ("""Area/ Surface Area:
+Traingle · · · · · : A = 1/2b * h
+Circle · · · · · · : A = πr^2
+Trapezoid  · · · · : A = 1/2(b1 + b2) * h
+Sphere · · · · · · : A = 4πr^2
+Square · · · · · · : A = s^2
+Rectangle  · · · · : A = l * w
+Parallelogram  · · : A = b * h
 """)
     
 def vollist():
     print ("""Volume Formulas:
-cube · · · · · · · : V = S^3
-rectangular prism  : V = lwh
-square pyramid · · : V = 1/3(b)^2 * h
-cylinder · · · · · : V = πr^3 * h
-cone · · · · · · · : V = 1/3πr^2 * h
-vol sphere · · · · : V = 4/3πr^3
+Cube · · · · · · · : V = S^3
+Rectangular Prism  : V = l * w * h
+Square Pyramid · · : V = 1/3(b)^2 * h
+Cylinder · · · · · : V = πr^3 * h
+Cone · · · · · · · : V = 1/3πr^2 * h
+Vol Sphere · · · · : V = 4/3πr^3
 """)
     
-def trilist():
+def triglist():
     print ("""Trig Formulas:
-pythagorean theorem: a^2 + b^2 = c^2
+Pythagorean Theorem: c^2 = a^2 + b^2
+Slope              : M = y2 - y1 / x2 - x1
+Point Distance     : d = sqrt([x2 - x1]^2 + [y2 - y1]^2)
+
 """)
     
 def vislist():
@@ -43,7 +53,7 @@ def vislist():
 def complist():
     salist()
     vollist()
-    trilist()
+    triglist()
     vislist()
 
 def resetprog():
@@ -54,6 +64,7 @@ def resetprog():
 
 def entirething():
     complist()
+    deci = 4
     while True:
         
         def rtri369():
@@ -107,7 +118,7 @@ def entirething():
                 a = float(input("Side A: "))
                 b = float(input("Side B: "))
                 h = math.sqrt(a**2 + b**2)
-                h = round(h,4)
+                h = round(h,deci)
                 print ("Hypotenuse: " + str(h) + " " + u)
                 resetprog()
 
@@ -115,90 +126,119 @@ def entirething():
         elif form == "triangle":
                 clogo()
                 u = input("Unit type: ")
-                b = int(input("Base: "))
-                h = int(input("Height: "))
+                b = float(input("Base: "))
+                h = float(input("Height: "))
                 a = (b*h)/2
-                a = round(a,4)
+                a = round(a,deci)
                 print ("Area: " + str(a) + " " + u + " squared")
                 resetprog()
 
         elif form == "circle":
                 clogo()
                 u = input("Unit type: ")
-                r = int(input("Radius: "))
+                r = float(input("Radius: "))
                 a = math.pi*r*r
-                a = round(a,4)
+                a = round(a,deci)
                 print ("Area: " + str(a) + " " + u + " squared")
                 resetprog()
 
         elif form == "trapezoid":
                 clogo()
                 u = input("Unit type: ")
-                b1 = int(input("Base 1: "))
-                b2 = int(input("Base 2: "))
-                h = int(input("Height: "))
+                b1 = float(input("Base 1: "))
+                b2 = float(input("Base 2: "))
+                h = float(input("Height: "))
                 a = (h*b1*b2)/2
-                a = round(a,4)
+                a = round(a,deci)
                 print ("Area: " + str(a) + " " + u + " squared")
                 resetprog()
 
         elif form == "sphere":
                 clogo()
                 u = input("Unit type: ")
-                r = int(input("Radius: "))
+                r = float(input("Radius: "))
                 s = 4*math.pi*r**2
-                s = round(s,4)
+                s = round(s,deci)
                 print ("Surface Area: " + str(s) + " " + u + " squared")
                 resetprog()
-                
+
+        elif form == "square":
+                clogo()
+                u = input("Unit type: ")
+                s = float(input("Side: "))
+                a = s**2
+                a = round(a,deci)
+                print ("Area: " + str(a) + " " + u + " squared")
+                resetprog()
+
+        elif form == "rectangle":
+                clogo()
+                u = input("Unit type: ")
+                l = float(input("Length: "))
+                w = float(input("Width: "))
+                a = l*w
+                a = round(a,deci)
+                print ("Area: " + str(a) + " " + u + " squared")
+                resetprog()
+
+        elif form == "parallelogram":
+                clogo()
+                u = input("Unit type: ")
+                b = float(input("Base: "))
+                h = float(input("Height: "))
+                a = b*h
+                a = round(a,deci)
+                print ("Area: " + str(a) + " " + u + " squared")
+                resetprog()
+
     #volume list
         elif form == "cube":
                 clogo()
                 u = input("Unit type: ")
-                s = int(input("Side length: "))
+                s = float(input("Side length: "))
                 s = s**3
-                s = round(s,4)
+                s = round(s,deci)
                 print ("Volume: " + str(s) + " " + u + " cubed")
                 resetprog()
 
         elif form == "rectangular prism":
                 clogo()
                 u = input("Unit type: ")
-                l = int(input("Length: "))
-                w = int(input("Width: " ))
-                h = int(input("Height: " ))
+                l = float(input("Length: "))
+                w = float(input("Width: " ))
+                h = float(input("Height: " ))
                 v = l*w*h
-                v = round(v,4)
+                v = round(v,deci)
                 print ("Volume: " + str(v) + " " + u + " cubed")
                 resetprog()
 
         elif form == "sqare pyramid":
                 clogo()
                 u = input("Unit type: ")
-                b = int(input("Base: "))
-                h = int(input("Height: "))
+                b = float(input("Base: "))
+                h = float(input("Height: "))
                 v = h*((1/3)*b**2)
-                v = round(v,4)
+                v = round(v,deci)
                 print ("Volume: " + str(v) + " " + u + " cubed")
                 resetprog()
                 
         elif form == "cylinder":
                 clogo()
                 u = input("Unit type: ")
-                r = int(input("Radius: "))
-                h = int(input("Height: "))
+                r = float(input("Radius: "))
+                h = float(input("Height: "))
                 v = h*(math.pi*(r**2))
-                v = round(v,4)
+                v = round(v,deci)
                 print ("Volume: " + str(v) + " " + u + " cubed")
                 resetprog()
                 
         elif form == "cone":
                 clogo()
                 u = input("Unit type: ")
-                h = int(input("Height: "))
-                r = int(input("Radius: "))
+                h = float(input("Height: "))
+                r = float(input("Radius: "))
                 v = h*(math.pi*(r**3))/3
-                v = round(v,4)
+                v = round(v,deci)
                 print ("Volume: " + str(v) + " " + u + " cubed")
                 resetprog()
                 
@@ -207,7 +247,7 @@ def entirething():
                 u = input("Unit type: ")
                 r = float(input("Radius: "))
                 v = (4/3)*(math.pi*(r**3))
-                v = round(v,4)
+                v = round(v,deci)
                 print ("Volume: " + str(v) + " " + u + " cubed")
                 resetprog()
 
@@ -217,7 +257,7 @@ def entirething():
                 o = float(input("Opposite: "))
                 a = float(input("Adjacent: "))
                 h = math.sqrt(o**2 + a**2)
-                h = round(h,4)
+                h = round(h,deci)
                 prove369()
                 resetprog()
 
@@ -226,7 +266,7 @@ def entirething():
                 o = float(input("Opposite: "))
                 a = float(input("Adjacent: "))
                 h = math.sqrt(o**2 + a**2)
-                h = round(h,4)
+                h = round(h,deci)
                 prove449()
                 resetprog()
 
@@ -234,12 +274,13 @@ def entirething():
         elif form == "#help":
                 clogo()
                 print ("""#help: Brings up this list.
-#logo: brings up the sick Unicode block logo
-#list: Brings up the list of all formulas.
-#salist: Brings up Surface Area Formula list
-#vollist: Brings up Volume Formula list
-#trilist: Brings up Trig Formula list
-#vislist: Brings up Visualizer list
+#logo  · : Brings up the sick Unicode block logo
+#list  · : Brings up the list of all formulas.
+#salist  : Brings up Surface Area Formula list
+#vollist : Brings up Volume Formula list
+#triglist: Brings up Trig Formula list
+#vislist : Brings up Visualizer list
+#settings: Brings up Settings
 """)
 
         elif form == "#list":
@@ -252,10 +293,9 @@ def entirething():
         elif form == "#info":
             clogo()
             print ("""This program will display a list of formulas to complete mathematical operations.")
-Select the name of the formula you require.")
+Select the name of the formula you require. Default is 4 decimal places, go to #settings to change.")
 """)
             
-
         elif form == "#salist":
             clogo()
             salist()
@@ -271,44 +311,58 @@ Select the name of the formula you require.")
         elif form == "#vislist":
             clogo()
             vislist()
+    #settings list
+        elif form == "#settings":
+            clogo()
+            print("""@deci: Configure the amount of decimal places.
+""")
+
+        elif form == "@deci":
+            clogo()
+            while True:
+                try:
+                    deci = int(input("Amount of decimal places: "))
+                    clogo()
+                    complist()
+                    break
+                except ValueError:
+                    print ("Pick an number from 0 - 13.")
 
         else:
             print ("Please select from #help or #list.")
 
+# automatic OS selection
 
-while True:
-    opsys = input("Win/Lin/Mac: ")
-    opsys = opsys.lower()
+if platform == "linux" or platform == "linux2":
+    def clear():
+        _ = os.system('clear')
+    def clogo():
+        clear()
+        logo()
+    clogo()
+    print ("Type '#help' for commands.")
+    print ("")
+    entirething()
+    
+elif platform == "darwin":
+    def clear():
+        _ = os.system('clear')
+    def clogo():
+        clear()
+        logo()
+    clogo()
+    print ("Type '#help' for commands.")
+    print ("")
+    entirething()
+    
+elif platform == "win32":
+    def clear():
+        _ = os.system('cls')
+    def clogo():
+        clear()
+        logo()
+    clogo()
+    print ("Type '#help' for commands.")
+    print ("")
+    entirething()
 
-    if opsys == "win":
-        def clear():
-            _ = os.system('cls')
-        def clogo():
-            clear()
-            logo()
-        clogo()
-        print ("Type '#help' for commands.")
-        print ("")
-        entirething()
-    elif opsys == "lin":
-        def clear():
-            _ = os.system('clear')
-        def clogo():
-            clear()
-            logo()
-        clogo()
-        print ("Type '#help' for commands.")
-        print ("")
-        entirething()
-    elif opsys == "mac":
-        def clear():
-            _ = os.system('clear')
-        def clogo():
-            clear()
-            logo()
-        clogo()
-        print ("Type '#help' for commands.")
-        print ("")
-        entirething()
-    else:
-        print ("Pick a proper OS my dude.")
